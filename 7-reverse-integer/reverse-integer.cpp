31d453eaa3d578c5958a2b76948e1efd545f4ac7
Krishna3455 <krishnaasingh991@gmail.com>
2025-05-27 11:00:24 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (82.02%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -5,7 +5,7 @@ public:
         while(x!=0)
         {
             digit=x%10;
-            if(rev>INT_MAX/10 | rev<INT_MIN/10) return 0;
+            if(rev>INT_MAX/10 || rev<INT_MIN/10) return 0;
             rev=rev*10+digit;
             x/=10;
         }   
